6cc65a5a7c1c886f121a347696306cf31b720efd
lucaspender56-lgtm <lucaspender56@gmail.com>
2026-07-29 14:26:07 +0000

    Providers, Variables, and Versions file creation.

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "name_prefix" {
+  description = "Prefix on every resource name so teammates don't collide in the shared account."
+  type        = string
+  default     = "defaultnameprefix"  
+}
