5b065643b1771b794efd46f3f09368d96b142b88
sarajoumaa85-lab <sarajoumaa85@gmail.com>
2026-07-29 15:03:13 +0000

    Add SNS email subscription and notification

--- main.tf
+++ main.tf
@@ -14,3 +14,8 @@ resource "aws_sns_topic" "notifications" {
 output "sns_topic_arn" {
   value = aws_sns_topic.notifications.arn
 }
+resource "aws_sns_topic_subscription" "email_notifications" {
+  topic_arn = aws_sns_topic.notifications.arn
+  protocol = "email"
+  endpoint = var.notification_email 
+}
